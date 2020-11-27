0e4fc5ca815a7247c2933516e4c7d8ae4ba22b1e
Sergei Smirnov <sergous@gmail.com>
2020-11-27 02:37:06 -0500

    Updates storeCore test

--- src/stores/storeCore.test.ts
+++ src/stores/storeCore.test.ts
@@ -1,15 +1,51 @@
 import { StoreCore, RootStore } from '.';
-import { createSolutionApiMock } from '../api';
+import { createSpyObj } from '../api';
 import { StoreCoreError } from '../errors';
+import { ApiModel, ParseObject } from '../models';
 
 describe('StoreCore', () => {
-    let storeCore: StoreCore;
+    let store: StoreCore;
+    const apiMock = createSpyObj('ApiMockSpy', [
+        'saveOne',
+        'updateOneAttr',
+        'deleteOne',
+        'deleteListItem',
+        'findAll',
+    ]);
+
     beforeEach(() => {
         const rootStore = new RootStore();
-        storeCore = new StoreCore(rootStore, createSolutionApiMock());
+        store = new StoreCore(rootStore, <ApiModel>(<unknown>apiMock));
+        apiMock.deleteOne.mockReturnValue(Promise.resolve);
+    });
+
+    it('should hold ref to root store', () => {
+        expect(store).toHaveProperty('rootStore');
+        expect(store.rootStore).toBeDefined();
+        expect(store.rootStore).toBeInstanceOf(RootStore);
+    });
+
+    it('should save an item', () => {
+        const item = new ParseObject();
+        store.saveOne(item);
+        expect(apiMock.saveOne).toHaveBeenCalled();
+    });
+
+    it('should update an item', () => {
+        const item = new ParseObject();
+        const query = 'When is the best place?';
+        store.updateOneAttr(item, 'query', query);
+        expect(apiMock.updateOneAttr).toHaveBeenCalled();
+    });
+
+    it('should delete an item', async () => {
+        const item = new ParseObject();
+        await store.deleteOne(item);
+        expect(apiMock.deleteOne).toHaveBeenCalled();
+        expect(apiMock.deleteListItem).toHaveBeenCalled();
     });
 
     it('should set ERROR', () => {
-        expect(storeCore.ERROR).toBe(StoreCoreError);
+        expect(store.ERROR).toBe(StoreCoreError);
     });
 });
